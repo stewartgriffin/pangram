1e23a13c6f5d4bd1056fc7626fd3fd3d94a30ffe
Ziajko <tomasz.ziajko@avl.com>
2016-11-30 19:53:16 +0100

    Refactor.

--- pangram.rb
+++ pangram.rb
@@ -4,9 +4,10 @@ end
 
 class Pangram
   def self.pangram?(phrase)
+    phrase_to_compare = phrase.downcase.each_char.to_a.uniq
     pangram = true
     ('a'..'z').to_a.each do |alphabet_chars|
-      unless phrase.downcase.each_char.to_a.include?(alphabet_chars)
+      unless phrase_to_compare.include?(alphabet_chars)
         pangram = false
         break
       end
