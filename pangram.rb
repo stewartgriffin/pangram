2c2439325edfc977a20190b698ce8203d9f92019
Ziajko <tomasz.ziajko@avl.com>
2016-11-30 19:40:27 +0100

    Pass: test_pangram_with_only_lower_case, test_missing_character_x, test_another_missing_character_x, test_pangram_with_underscores, test_pangram_with_numbers, test_missing_letters_replaced_by_numbers

--- pangram.rb
+++ pangram.rb
@@ -3,7 +3,14 @@ module BookKeeping
 end
 
 class Pangram
-  def self.pangram?(*)
-    false
+  def self.pangram?(phrase)
+    pangram = true
+    ('a'..'z').to_a.each do |alphabet_chars|
+      unless phrase.each_char.to_a.include?(alphabet_chars)
+        pangram = false
+        break
+      end
+    end
+    pangram
   end
 end
